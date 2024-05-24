63237ccb0e022599d6eb95b8f12eab54526b0658
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-05-24 09:34:24 +0200

    Mise à jour de la vue et du contrôleur

--- src/main/java/fr/univartois/butinfo/ihm/flatcraft/controller/InventaireControleur.java
+++ src/main/java/fr/univartois/butinfo/ihm/flatcraft/controller/InventaireControleur.java
@@ -3,11 +3,15 @@ package fr.univartois.butinfo.ihm.flatcraft.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class InventaireControleur {
 
     @FXML
     private Label descriptionItem;
+
+    @FXML
+    private ListView<?> listItems;
 
     @FXML
     private Label nomItem;
