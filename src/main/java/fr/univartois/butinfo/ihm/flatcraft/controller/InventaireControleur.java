87e78d51e1096904e50709404bd7f2ba554d309f
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-05-24 09:32:45 +0200

    Ajout du contrôleur de l'inventaire

---
+++ src/main/java/fr/univartois/butinfo/ihm/flatcraft/controller/InventaireControleur.java
@@ -0,0 +1,20 @@
+package fr.univartois.butinfo.ihm.flatcraft.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InventaireControleur {
+
+    @FXML
+    private Label descriptionItem;
+
+    @FXML
+    private Label nomItem;
+
+    @FXML
+    void onRetourButtonClick(ActionEvent event) {
+
+    }
+
+}
